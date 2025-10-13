66a5ba063888bb299f30312f2da93ff3db4d711b
harmanacc <armanabdiacc@gmail.com>
2025-10-13 11:15:15 +0330

    calling number

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,18 @@ const app = new Hono();
 //   return c.text('Hello Hono!')
 // })
 
-app.get("/:number", (c) => {
-  const number = c.req.param("number");
-  const result = Number(number) * 2;
-  return c.text(result.toString());
+app.get("/", (c) => {
+  const callingNumber = c.req.query("callingnumber");
+  if (!callingNumber) {
+    return c.json({
+      Status: false,
+      Error: "Calling Number is required",
+    });
+  }
+  return c.json({
+    Status: true,
+    CalledNumber: "09124166379",
+  });
 });
 
 export default app;
