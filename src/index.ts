928e8f6e83d70e81902ecfc34311e31737d5bf41
harmanacc <armanabdiacc@gmail.com>
2025-10-15 10:42:00 +0330

    update to only accept one number

--- src/index.ts
+++ src/index.ts
@@ -9,53 +9,65 @@ const app = new Hono();
 const generate9Number = (number: string) => "9" + number;
 
 app.get("/", (c) => {
-	try {
-		const callingNumber =
-			c.req.query("callingnumber") ||
-			c.req.query("caller") ||
-			c.req.query("CallingNumber");
-		if (!callingNumber) {
-			return c.json(
-				{
-					error: "Calling Number is required (use 'callingnumber', 'caller', or 'CallingNumber' query parameter)",
-					status: false,
-				},
-				{ status: 400 }
-			);
-		}
+  try {
+    const callingNumber =
+      c.req.query("callingnumber") ||
+      c.req.query("caller") ||
+      c.req.query("CallingNumber");
 
-		const peymanNumber = "09127154605";
+    const peymanNumber = "09127154605";
+    const mohandesKaniNumber = "09127808487";
+    // mirayi number
+    const returningNumber = "9" + "09124166379";
 
-		const mohandesKaniNumber = "09127808487";
+    if (!callingNumber) {
+      return c.json(
+        {
+          error:
+            "Calling Number is required (use 'callingnumber', 'caller', or 'CallingNumber' query parameter)",
+          status: false,
+        },
+        { status: 400 }
+      );
+    }
+    if (callingNumber != peymanNumber) {
+      return c.json(
+        {
+          error:
+            "Calling Number is not valid. you will be redirected to voip portal",
+          status: false,
+        },
+        { status: 400 }
+      );
+    }
 
-		if (callingNumber == peymanNumber) {
-			return c.json(
-				{
-					calledNumber: generate9Number(mohandesKaniNumber),
-					status: true,
-				},
-				{ status: 200 }
-			);
-		}
+    // if (callingNumber == peymanNumber) {
+    return c.json(
+      {
+        calledNumber: generate9Number(mohandesKaniNumber),
+        status: true,
+      },
+      { status: 200 }
+    );
+    // }
 
-		const returningNumber = "9" + "09124166379";
-		// Additional validation can be added here (e.g., regex for phone number format)
-		return c.json(
-			{
-				calledNumber: returningNumber,
-				status: true,
-			},
-			{ status: 200 }
-		);
-	} catch (error) {
-		return c.json(
-			{
-				error: "Internal server error",
-				status: false,
-			},
-			{ status: 500 }
-		);
-	}
+    // // Additional validation can be added here (e.g., regex for phone number format)
+    // return c.json(
+    //   {
+    //     calledNumber: returningNumber,
+    //     status: true,
+    //   },
+    //   { status: 200 }
+    // );
+  } catch (error) {
+    return c.json(
+      {
+        error: "Internal server error",
+        status: false,
+      },
+      { status: 500 }
+    );
+  }
 });
 
 export default app;
